7918517039546f83ebb960ccd5bff19fd782cd7a
MauricioMurga <mauricio_murga@hotmail.com>
2014-09-24 13:47:57 -0500

    update changelog

--- src/com/conekta/Conekta.java
+++ src/com/conekta/Conekta.java
@@ -14,7 +14,7 @@ public abstract class Conekta {
     public static String apiKey;
     public static String apiBase = "https://api.conekta.io";
     public static String apiVersion = "0.3.0";
-    public static final String VERSION = "1.0.2";
+    public static final String VERSION = "1.0.3";
 
     public static void setApiKey(String apiKey) {
         Conekta.apiKey = apiKey;
