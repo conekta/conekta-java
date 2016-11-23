c9d28b43abd063ae0c58566d8ebb39bcc492d43c
Mauricio Murga <mauricio@conekta.io>
2016-11-23 16:07:07 -0600

    clean and build

--- build/classes/com/conekta/Error.rs
+++
@@ -1,8 +0,0 @@
-com.conekta.NoConnectionError
-com.conekta.AuthenticationError
-com.conekta.ResourceNotFoundError
-com.conekta.Error
-com.conekta.ParameterValidationError
-com.conekta.ProcessingError
-com.conekta.MalformedRequestError
-com.conekta.ApiError
