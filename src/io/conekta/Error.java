4a2cd41d8b624e6a52ae6ed8aec12333dd063639
Eduardo Enriquez <eduardo@MacBook-Air-de-Eduardo-E.local>
2017-11-09 10:51:00 -0600

    Fix/typo in params parameter at error class

--- src/io/conekta/Error.java
+++ src/io/conekta/Error.java
@@ -66,7 +66,7 @@ public class Error extends Exception {
         
         this.type = type;
         this.code = code;
-        this.params = params;
+        this.params = param;
     }
 
     @Override
